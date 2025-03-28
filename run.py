0d83b203c65d22f941cad5067de5175c409def75
Mungai Munene <mungai.munene@gmail.com>
2025-03-28 20:33:49 +0300

    Updated run.py to handle dynamic port for deployment

--- run.py
+++ run.py
@@ -1,7 +1,11 @@
-# run.py
 from app import create_app
+import os
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    # Use the PORT environment variable for dynamic port (on Render or similar platforms)
+    port = int(os.environ.get('PORT', 5000))  # Default to 5000 if no port is provided (local)
+    
+    # Run the app with the specified host and port
+    app.run(host='0.0.0.0', port=port, debug=True)
